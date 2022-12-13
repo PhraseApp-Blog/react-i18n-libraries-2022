88d9059bc5fbbfd911d7a216ba3bac05b4ca0305
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-12-13 12:31:47 +0100

    Adding i18next demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import logo from "./logo.svg";
+import "./App.css";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        demo: "This demo app was internationalized by i18next",
+      },
+    },
+    ar: {
+      translation: {
+        demo: "تم تدويل هذا التطبيق التجريبي بواسطة آي ايتين نكست",
+      },
+    },
+  },
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  function setLocale(newLocale) {
+    i18n.changeLanguage(newLocale);
+  }
+
   return (
     <div className="app">
       <header className="app__header">
         <img src={logo} className="app__logo" alt="logo" />
+        <a href="#" className="app__link" onClick={() => setLocale("en")}>
+          English
+        </a>
+        <a href="#" className="app__link" onClick={() => setLocale("ar")}>
+          Arabic
+        </a>
       </header>
 
       <main className="app__main">
-        <p>This demo React app will be internationalized by various i18n
-          libaries.
-        </p>
+        <p>{t("demo")}</p>
       </main>
     </div>
   );
