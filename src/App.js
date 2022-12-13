1f2157c19e9c6a80b1caa7fdbbf5eab20cf979f3
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-12-13 13:18:20 +0100

    Adding react-intl demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { IntlProvider, FormattedMessage } from "react-intl";
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const messages = {
+  en: {
+    demo: "This demo app was internationalized by react-intl",
+  },
+  ar: {
+    demo: "تم تدويل هذا التطبيق التجريبي بواسطة رياكت إنتل",
+  },
+};
 
 function App() {
-  return (
-    <div className="app">
-      <header className="app__header">
-        <img src={logo} className="app__logo" alt="logo" />
-      </header>
+  const [locale, setLocale] = useState("en");
 
-      <main className="app__main">
-        <p>This demo React app will be internationalized by various i18n
-          libaries.
-        </p>
-      </main>
-    </div>
+  return (
+    <IntlProvider messages={messages[locale]} locale={locale}>
+      <div className="app">
+        <header className="app__header">
+          <img src={logo} className="app__logo" alt="logo" />
+          <a href="#" className="app__link" onClick={() => setLocale("en")}>
+            English
+          </a>
+          <a href="#" className="app__link" onClick={() => setLocale("ar")}>
+            Arabic
+          </a>
+        </header>
+
+        <main className="app__main">
+          <p>
+            <FormattedMessage id="demo" />
+          </p>
+        </main>
+      </div>
+    </IntlProvider>
   );
 }
 
