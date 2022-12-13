405f5d4bab43be142643266cba12b2307777aab1
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-12-13 12:08:29 +0100

    Adding app content and updating styles.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app__header">
+        <img src={logo} className="app__logo" alt="logo" />
       </header>
+
+      <main className="app__main">
+        <p>This demo React app will be internationalized by various i18n
+          libaries.
+        </p>
+      </main>
     </div>
   );
 }
