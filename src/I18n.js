cb33746dead507bb5478ac6860079ea560a98650
Mohammad Ashour <ashour.mohammad@gmail.com>
2022-12-14 18:17:37 +0100

    Adding plural config to avoid console warnings/errors.

--- src/I18n.js
+++ src/I18n.js
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
 import { I18nProvider } from "@lingui/react";
 import { i18n } from "@lingui/core";
+import { en as enPlurals, ar as arPlurals } from "make-plural/plurals";
 import { messages as enMessages } from "./locales/en-US/messages";
 import { messages as arMessages } from "./locales/ar-EG/messages";
 
-const messages = {
-  "en-US": enMessages,
-  "ar-EG": arMessages,
+const localeConfig = {
+  "en-US": { messages: enMessages, plurals: enPlurals },
+  "ar-EG": { messages: arMessages, plurals: arPlurals },
 };
 
 const defaultLocale = "en-US";
 
 function loadLocale(locale) {
-  i18n.load(locale, messages[locale]);
+  const { plurals, messages } = localeConfig[locale];
+
+  i18n.loadLocaleData(locale, { plurals });
+  i18n.load(locale, messages);
   i18n.activate(locale);
 }
 
